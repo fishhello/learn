c2f9002f746334766080bff2b9c4bab7d3357e3b
fishhello <2874497884@qq.com>
2019-02-18 18:07:54 +0800

    添加python封装多态继承组合测试代码

--- python/run.py
+++ python/run.py
@@ -179,3 +179,16 @@ rectangle = Rectangle(30, 40)
 print(rectangle.area())
 rectangle.change_size(40, 50)
 print(rectangle.area())
+
+
+class Shape():
+    def __init__(self, w, l):
+        self.width = w
+        self.len = l
+
+    def print_size(self):
+        print(" {} by {}".format(self.width, self.len))
+
+
+my_shape = Shape(20, 25)
+my_shape.print_size()
